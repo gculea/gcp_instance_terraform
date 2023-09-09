3d031fc8e0d2370b3cd7854a2dcfade7ef479398
gculea <91122753+gculea@users.noreply.github.com>
2023-09-10 02:43:19 +0300

    Update vars.tf
    Added comments

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("mgccloud-c3b18a7dd9ab.json")
-  project     = "mgccloud"
+  project     = "mgccloud" # Change to your gcp project
   region      = "europe-west1"  # Change to your desired region
 }
 
@@ -11,11 +11,11 @@ variable "number_of_vms" {
 
 variable "vm_flavor" {
   description = "Flavor of the VM."
-  default     = "n1-standard-1"
+  default     = "n1-standard-1" # Change to your desired VM type
 }
 
 variable "vm_image" {
   description = "Image for the VM."
-  default     = "ubuntu-2004-lts"
+  default     = "ubuntu-2004-lts" # Change to your desired VM image
 }
 
